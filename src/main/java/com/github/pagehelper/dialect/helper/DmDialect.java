fdf1a8e74766640b3ddf53d5c311b130dd1235a3
liyong <1506100524@qq.com>
2026-09-18 17:05:14 +0800

    docs: 补充 DmDialect 许可证头并调整注释

--- src/main/java/com/github/pagehelper/dialect/helper/DmDialect.java
+++ src/main/java/com/github/pagehelper/dialect/helper/DmDialect.java
@@ -1,5 +1,28 @@
-package com.github.pagehelper.dialect.helper;
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2023 abel533@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
+package com.github.pagehelper.dialect.helper;
 
 import com.github.pagehelper.Page;
 import com.github.pagehelper.dialect.AbstractHelperDialect;
@@ -10,8 +33,6 @@ import org.apache.ibatis.mapping.MappedStatement;
 import java.util.Map;
 
 /**
- * DM 方言，分页使用偏移量分页，提高分页性能
- *
  * @author liyong
  */
 public class DmDialect extends AbstractHelperDialect {
@@ -32,7 +53,7 @@ public class DmDialect extends AbstractHelperDialect {
     public String getPageSql(String sql, Page page, CacheKey pageKey) {
         StringBuilder sqlBuilder = new StringBuilder(sql.length() + 64);
         sqlBuilder.append(sql);
-        sqlBuilder.append(" OFFSET ? ROWS FETCH NEXT ? ROWS ONLY");
+        sqlBuilder.append("\n OFFSET ? ROWS FETCH NEXT ? ROWS ONLY");
         return sqlBuilder.toString();
     }
 
